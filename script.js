ca1482e5a762d6ee5ffdf22d31f1df480c1aae8f
Sandeep Reddy <msande1996@gmail.com>
2017-01-16 14:37:57 +0530

    Add Unsplash API and tags

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ jQuery(function($){
 	var appid="4872749c32f138daf93f7d4f8644f85e";
 	var celcius=true;
 
+	var temp=NaN;
 
 	var tempScales=['Desert','City','Fire','Snow'];
 
@@ -26,27 +27,69 @@ jQuery(function($){
 		fetchWeatherUrl+='&lat='+lat;
 		fetchWeatherUrl+='&lon='+long;
 		fetchWeatherUrl+='&units=metric';
+		console.log("URL= "+fetchWeatherUrl);
 		fetchWeather(fetchWeatherUrl);
 	}
 
 
 	function fetchWeather(fetchWeatherUrl){
 		fetch(fetchWeatherUrl)
-			.then(function(response){
-				return response.json();
-			}).then(function(json){
-				setElements(json);
-			});
+		.then(function(response){
+			return response.json();
+		}).then(function(json){
+			setElements(json);
+			showHideItems();
+		});
+	}
+
+	function showHideItems(){
+		$('.loadin').hide();
+		$('.unit_tog').show();
 	}
 
 
 	function setElements(weather){
-		console.log(weather);
+		temp=weather.main.temp;
 		$('.city_details').html(weather.name+','+weather.sys.country+'');
-		$('.temp_details').html('Temperature : '+weather.main.temp+' '+degre+'C');
+		setTemp();
 		$('.speed_details').html('Speed : '+weather.wind.speed+' knots');
 		$('.weat_desc').html(weather.weather[0].description);
 		setWeatherIcon(weather.weather[0].main,weather.sys);
+		$("body").css("background",'linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)),url("'+getQueryUrl(weather)+'")');
+	}
+
+	function getQueryUrl(wejson){
+		var queryUrl="https://source.unsplash.com/";
+		queryUrl+=$(window).width()+"x"+$(window).height()+"/?";
+		var temp=wejson.main.temp;
+		if(temp>40){
+			queryUrl+=tempScales[0];
+		}else if(temp>20){
+			queryUrl+=tempScales[1];
+		}else if(temp>0){
+			queryUrl+=tempScales[2];
+		}else{
+			queryUrl+=tempScales[3];
+		}
+		if(wejson.weather[0].main=="Clear"){
+			var millis=(new Date()).getTime();
+			if(wejson.sys.sunrise<millis && millis<wejson.sys.sunset){
+				queryUrl+=",Night";
+			}
+		}else{
+			queryUrl+=","+wejson.weather[0].main;
+		}
+		console.log(queryUrl);
+		return queryUrl;
+	}
+
+	function setTemp(){
+		$('.temp_details').html('Temperature : '+temp+' '+degre);
+		if(celcius){
+			$('.temp_details').append('C');
+		}else{
+			$('.temp_details').append('F');
+		}
 	}
 
 	function setWeatherIcon(condition,sys){
@@ -59,7 +102,7 @@ jQuery(function($){
 			icon+=icons[3];
 		}else{
 			var millis=(new Date()).getTime();
-			if(millis>sys.sunset || millis<sys.sunrise){
+			if(millis>sys.sunset && millis<sys.sunrise){
 				icon+=icons[1];
 			}else{
 				icon+=icons[0];
@@ -69,6 +112,28 @@ jQuery(function($){
 		icon+='"></i>';
 		$('.weat_icon').html(icon);
 	}
+
+	function convertToFar(){
+		temp=temp*1.8+32;
+		temp= +temp.toFixed(2);
+		celcius=false;
+		setTemp();
+	}
+
+	function convertToCel(){
+		temp=(temp-32)/1.8;
+		temp= +temp.toFixed(2);
+		celcius=true;
+		setTemp();
+	}
+
+	$('#uni_to').change(function(){
+		if(celcius){
+			convertToFar();
+		}else{
+			convertToCel();
+		}
+	});
 
 	getLocation();
 });
